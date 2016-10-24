ed6bcf522f69cc9def47dd015eb3a12f10f0dbbf
raghuma sama <raghuma sama>
2016-10-24 15:49:43 -0400

    first commit

---
+++ lengthandwidth/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lengthandwidth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Double area;
+        Double perimeter;
+            string continuee;
+            do
+            {
+                Console.WriteLine("enter the length of the room");
+                string input = Console.ReadLine();
+                double length = Convert.ToDouble(input);
+                Console.WriteLine("enter the width of the room");
+                string info = Console.ReadLine();
+                double width = Convert.ToDouble(info);
+                area = length * width;
+                Console.WriteLine("the area is " + area);
+                perimeter = (2 * length) + (2 * width);
+                Console.WriteLine("the perimeter is " + perimeter);
+              
+                Console.WriteLine("you want to continue y/n ");
+                 continuee = Console.ReadLine();
+            }
+            while( continuee =="y");
+
+
+
+
+
+        }
+    }
+}
